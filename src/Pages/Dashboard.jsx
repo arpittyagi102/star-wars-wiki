53bbc423a9a2b0756c6a9d2cfb5257e8f121845f
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-29 23:48:05 +0530

    using nested css

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,7 +2,10 @@ export default function Dashboard(){
 
     return (
       <>
-        <img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" width="72" alt="star wars logo" />
+        <div className="Upper-bar" style={{padding:'20px'}}>
+            <img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" width="72" alt="star wars logo" />
+            <div className="search-bar"></div>
+        </div>
       </>
     )
   }
