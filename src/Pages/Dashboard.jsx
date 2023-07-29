dc8f9f8b2fc31014cd10a6f68755e2d1840e9d36
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-29 22:57:00 +0530

    creating Dashboard

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard(){
+
+    return (
+      <>
+        <img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" width="72" alt="star wars logo" />
+      </>
+    )
+  }
